0301470f15caa06aa0b55d76f514f69229fee3d0
edahg <edahg.g@gmail.com>
2020-12-17 00:04:03 -0500

    token y middleware

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,31 +1,24 @@
-const config = require('../secret/config.js');
 const db = require('../models');
-var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
-const tokenizer = require('../services/token')
-const { restart } = require('nodemon');
+const tokenService = require('../services/token')
 
-exports.login = (req, res) => {
-    db.Usuario.findOne({
-        where: {
+exports.login = async(req, res) => {
+    try{
+        const user = await db.Usuario.findOne({where: {
             email: req.body.email
-        }
-    }).then(user => {
-        if (!user){
-            console.log("User not found.")
+        }})
+        if(user){
+            const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+            if (passwordIsValid){
+                const token = await tokenService.encode(user.id, user.rol)
+                res.status(200).send({ auth: true, tokenReturn: token })
+            }else{
+                return res.status(401).send({ auth: false, tokenReturn: null, reason: "Invalid Password!"})
+            }
+        }else{
             return res.status(404).send('User Not Found.');
         }
-
-        var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-        if (!passwordIsValid){
-            return res.status(401).send({ auth: false, tokenReturn: null, reason: "Invalid Password!"})
-        }
-
-        var token = tokenizer.tokenEncode(user);
-
-        res.status(200).send({ auth: true, tokenReturn: token })
-    }).catch( err => {
+    }catch(err) {
         res.status(500).send('Error -> ' + err)
-    })
-    
+    }
 }
